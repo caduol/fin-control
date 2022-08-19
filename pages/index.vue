10f28a9a6ff37b377b3ab6a5d7b9d393b88a1993
Cadu <eduardo.w3c@gmail.com>
2022-08-19 01:02:21 -0300

    :art: Buscando relacao de transacoes com categoria para listagem, formatacao de data

--- pages/index.vue
+++ pages/index.vue
@@ -22,14 +22,133 @@
       </div>
     </div>
 
-    <div class="mt-4">
-      <div class="space-y-8">
+    <!-- <div
+      class="flex items-center px-5 py-6 bg-white rounded-lg shadow my-3"
+      v-for="transaction in transactionGrouped"
+      :key="transaction.id"
+    >
+      <div class="flex items-center space-x-5">
         <div>
-          <div class="mb-1">
-            <div class="font-bold text-sm">04 de Jan</div>
+          <div>
+            <div
+              class="inline-flex items-center px-2.5 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800"
+            >
+              {{ transaction.category.name }}
+            </div>
           </div>
 
-          <div class="space-y-3">
+          <div class="mt-1.5">
+            {{ transaction.date }} - {{ transaction.description }}
+          </div>
+        </div>
+      </div>
+
+      <div class="flex items-center space-x-4 ml-auto">
+        <div class="flex items-center">
+          <svg
+            class="w-4 h-4 text-red-600"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M20 12H4"
+            ></path>
+          </svg>
+
+          <div class="font-bold">R$ 43,02</div>
+        </div>
+
+        <button>
+          <svg
+            class="w-4 h-4"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 9l-7 7-7-7"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div> -->
+    <div class="mt-4">
+      <div class="space-y-8">
+        <div v-for="(group, index) in transactionGrouped" :key="index">
+          <div class="mb-1">
+            <div class="font-bold text-sm">{{ formatDate(index) }}</div>
+          </div>
+          <div
+            class="space-y-3"
+            v-for="transaction in group"
+            :key="transaction.id"
+          >
+            <div
+              class="flex items-center px-5 py-6 bg-white rounded-lg shadow my-2"
+            >
+              <div class="flex items-center space-x-5">
+                <div>
+                  <div>
+                    <div
+                      class="inline-flex items-center px-2.5 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800"
+                    >
+                      {{ transaction.category.name }}
+                    </div>
+                  </div>
+
+                  <div class="mt-1.5">{{ transaction.description }}</div>
+                </div>
+              </div>
+
+              <div class="flex items-center space-x-4 ml-auto">
+                <div class="flex items-center">
+                  <svg
+                    class="w-4 h-4 text-red-600"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M20 12H4"
+                    ></path>
+                  </svg>
+
+                  <div class="font-bold">R$ {{ transaction.amount }}</div>
+                </div>
+
+                <button>
+                  <svg
+                    class="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M19 9l-7 7-7-7"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="space-y-3">
             <div class="px-5 py-6 bg-white rounded-lg shadow">
               <div class="flex items-center">
                 <div class="flex items-center space-x-5">
@@ -120,60 +239,8 @@
               </div>
             </div>
 
-            <div class="flex items-center px-5 py-6 bg-white rounded-lg shadow">
-              <div class="flex items-center space-x-5">
-                <div>
-                  <div>
-                    <div
-                      class="inline-flex items-center px-2.5 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800"
-                    >
-                      Software
-                    </div>
-                  </div>
 
-                  <div class="mt-1.5">Pagamento de boleto</div>
-                </div>
-              </div>
-
-              <div class="flex items-center space-x-4 ml-auto">
-                <div class="flex items-center">
-                  <svg
-                    class="w-4 h-4 text-red-600"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M20 12H4"
-                    ></path>
-                  </svg>
-
-                  <div class="font-bold">R$ 43,02</div>
-                </div>
-
-                <button>
-                  <svg
-                    class="w-4 h-4"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M19 9l-7 7-7-7"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
+          </div> -->
         </div>
 
         <div>
@@ -235,60 +302,6 @@
                 </button>
               </div>
             </div>
-
-            <div class="flex items-center px-5 py-6 bg-white rounded-lg shadow">
-              <div class="flex items-center space-x-5">
-                <div>
-                  <div>
-                    <div
-                      class="inline-flex items-center px-2.5 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800"
-                    >
-                      Software
-                    </div>
-                  </div>
-
-                  <div class="mt-1.5">Pagamento de boleto</div>
-                </div>
-              </div>
-
-              <div class="flex items-center space-x-4 ml-auto">
-                <div class="flex items-center">
-                  <svg
-                    class="w-4 h-4 text-red-600"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M20 12H4"
-                    ></path>
-                  </svg>
-
-                  <div class="font-bold">R$ 43,02</div>
-                </div>
-
-                <button>
-                  <svg
-                    class="w-4 h-4"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M19 9l-7 7-7-7"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
@@ -298,6 +311,7 @@
 
 <script>
 import AppButton from "~/components/Ui/AppButton";
+import { groupBy, orderBy } from "lodash";
 import AppFormInput from "~/components/Ui/AppFormInput";
 import AppFormLabel from "~/components/Ui/AppFormLabel";
 import AppFormSelect from "~/components/Ui/AppFormSelect";
@@ -319,11 +333,20 @@ export default {
       isAdding: false,
     };
   },
-
+  computed: {
+    transactionGrouped() {
+      return groupBy(orderBy(this.transactions, "date", "desc"), "date");
+    },
+  },
   async asyncData({ store }) {
     return {
       transactions: await store.dispatch("transactions/getTransactions"),
     };
+  },
+  methods: {
+    formatDate(date) {
+      return this.$dayjs(date).format("DD/MM/YYYY");
+    },
   },
 };
 </script>
